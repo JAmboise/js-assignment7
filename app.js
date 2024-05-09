8f4ccee8f6f9f09ebd5d3768fb349b2ed1fba0a7
Jude Amboise <amboiseyide@gmail.com>
2024-05-09 11:29:31 -0500

    Assignment 7 completed

--- app.js
+++ app.js
@@ -17,14 +17,28 @@ app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
 
-
+//
 
 // GET /api/todos
+app.get('/api/todos', (_, response) => {
+	response.json(todos)
+})
 
 // POST /api/todos
+app.post('/api/todos', (request, response) => {
+	const { item } = request.body
+	const id = todos.length + 1
+	todos.push({ id, item, complete: false })
+	response.json({ id, item, complete: false })
+})
 
-// PUT /api/todos/:id
-
+// PUT /api/todos/:id 
+app.put('/api/todos/:id', (request, response) => {
+	const id = parseInt(request.params.id)
+	const todo = todos.find(todo => todo.id === id)
+	todo.complete = !todo.complete
+	response.json(todo)
+})
 
 
 const message = `Server running: http://localhost:${port}`
