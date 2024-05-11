0b6098eb1b5afbd67ea4434bd9c111290cd8aabc
Jude Amboise <amboiseyide@gmail.com>
2024-05-10 21:57:25 -0500

    Added all endpoints, check the checked endpoint

---
+++ api-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { MongoClient, ObjectID} = require('mongodb'); 
+
+
+const url = process.env.MONGODB_URI || require('./secrets/mongodb.json').url //This is the connection string
+const client = new MongoClient(url) 
+
+
+// This route function should return a list of all tasks in the "To Do" list.
+const getCollection = async (dbName, collectionName) => {
+	await client.connect()
+	return client.db(dbName).collection(collectionName)
+}
+
+//**ENDPOINTS**
+
+
+// GET /api/todos //Read all todos, This route should return a list of all tasks in the "To Do" list.
+router.get('/', async (_, response) => {
+    const collection = await getCollection('todo-api', 'todos')
+    const todos = await collection.find().toArray()
+	response.json(todos) 
+})
+
+// POST /api/todos // Create a todos, This route should allow users to add a new task to the "To Do" list.
+router.post('/', async (request, response) => {
+    const { body } = request
+    const { item, complete} = body
+    const todo = { item, complete: false}
+
+    const collection = await getCollection('todo-api', 'todos')
+    const result = await collection.insertOne(todo)
+    response.json(result)
+}) 
+
+// PUT /api/todos/:id // Update a todos, This route should allow users to mark a task as complete.
+router.put('/:id', async (request, response) => {
+    const { body, params } = request
+    const { id } = params
+    const { complete } = body
+    const collection = await getCollection('todo-api', 'todos')
+    const result = await collection.updateOne({ _id: ObjectID(id) }, { $set: { complete } })
+    response.json(result)
+
+
+
+
+    // const { body, params } = request
+    // const { id } = params
+    // const { complete } = body
+    // const todo = todo.find(todo => todo.id === id)
+    // todo.complete = !todo.complete
+
+    // const collection = await getCollection('todo-api', 'todos')
+    // const result = await collection.updateOne({ _id: ObjectID(id) }, { $set: todo })
+    // response.json(result)
+
+
+
+	//const id = parseInt(request.params.id)
+	// const todo = todo.find(todo => todo.id === id)
+	// todo.complete = !todo.complete
+	// response.json(todo)
+}) 
+// Assignment 8 endpoints
+
+
+module.exports = router
+
+
+// const todos = [
+// 	{ id: 1, item: 'Learn JavaScript', complete: false },
+// 	{ id: 2, item: 'Learn Express', complete: false },
+// 	{ id: 3, item: 'Build a To Do App', complete: false }
+// ]
--- app.js
+++ app.js
@@ -1,49 +1,23 @@
-
 const express = require('express')
+const apiRoutes = require('./api-routes')
 const app = express()
+
+app.use(express.json())
+app.use('/api/todos', apiRoutes)
 
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// **Middleware to serve static files from the 'public' directory**
+const path = require('path')
+const root = path.join(__dirname, 'public')
 app.use(express.static('public'))
 
-const todos = [
-	{ id: 1, item: 'Learn JavaScript', complete: false },
-	{ id: 2, item: 'Learn Express', complete: false },
-	{ id: 3, item: 'Build a To Do App', complete: false }
-]
-
-app.get('/', (_, response) => {
-	response.sendFile('index.html', { root })
+app.get('/', ( request, response) => {
+    response.sendFile('index.html', { root }) //
 })
 
 
-
-
-// GET /api/todos //Read all todos, This route should return a list of all tasks in the "To Do" list.
-app.get('/api/todos', (_, response) => {
-	response.json(todos) 
-})
-
-// POST /api/todos // Create a todos, This route should allow users to add a new task to the "To Do" list.
-app.post('/api/todos', (request, response) => {
-	const { item } = request.body
-	const id = todos.length + 1
-	todos.push({ id, item, complete: false })
-	response.json({ id, item, complete: false })
-}) 
-
-// PUT /api/todos/:id // Update a todos, This route should allow users to mark a task as complete.
-app.put('/api/todos/:id', (request, response) => {
-	const id = parseInt(request.params.id)
-	const todo = todos.find(todo => todo.id === id)
-	todo.complete = !todo.complete
-	response.json(todo)
-}) 
-
-
-const message = `Server running: http://localhost:${port}`
-app.listen(port, () => console.log(message))
+app.listen(port, () => console.log(`Server running: http://localhost:${port}`))
 
 
 //Practice
